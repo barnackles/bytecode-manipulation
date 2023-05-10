42a40a9a6e83c577e30a5fdad225098eb6c39824
barnackles <p.martewicz@yahoo.com>
2023-05-10 13:57:11 +0200

    Book and ReflectionTest classes added.

--- src/main/java/com/kodilla/bytecode/KodillaByteManipulationApplication.java
+++ src/main/java/com/kodilla/bytecode/KodillaByteManipulationApplication.java
@@ -1,4 +1,4 @@
-package org.example.kodillabytemanipulation;
+package com.kodilla.bytecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/kodilla/bytecode/reflection/ReflectionTest.java
@@ -0,0 +1,8 @@
+package com.kodilla.bytecode.reflection;
+
+public class ReflectionTest {
+
+    public static void main(String[] args) {
+        Class<Book> descriptor = Book.class;
+    }
+}
