a0fe24aae59c4a30b3940b3ccaef02e4861c0f80
barnackles <p.martewicz@yahoo.com>
2023-05-10 14:20:13 +0200

    Project rebuilt.

--- src/main/java/com/kodilla/bytecode/reflection/ReflectionTest.java
+++ src/main/java/com/kodilla/bytecode/reflection/ReflectionTest.java
@@ -1,8 +1,13 @@
 package com.kodilla.bytecode.reflection;
 
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
 
     public static void main(String[] args) {
         Class<Book> descriptor = Book.class;
+        for (Method method : descriptor.getMethods()) {
+            System.out.println(method.getName());
+        }
     }
 }
